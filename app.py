7531aaba18f527af1307d6003481da7c6e5cf282
Vitor Sá <vitorsilvasa87@gmail.com>
2026-04-05 18:07:29 -0300

    Implementado sistema de login
    Sistema de login com leitura no banco de dados onde o usuario nao pode entrar  sem login

--- app.py
+++ app.py
@@ -5,52 +5,72 @@ app = Flask(__name__)
 app.secret_key = '0'
 @app.route('/')
 def home():
-    return render_template('cadastro.html')
-
-@app.route('/cadastro', methods=['POST'])
-def cadastro():
-    try:
-        usuario = request.form['usuario']
-        email = request.form['cremail']
-        senha = request.form['crsenha']
-        
-        if not usuario or not email or not senha:
-            return 'Todos os campos são obrigatórios!'
-        
-        conexao = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='guriSQL_',
-            database='expotech'
+    return render_template('login.html')
+@app.route('/login', methods=['GET', 'POST'])  
+def login():
+    if request.method == 'POST':
+        email= request.form['email']
+        senha= request.form['senha']
+        conexao= mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='guriSQL_',
+        database='expotech'
         )
-        
         cursor = conexao.cursor()
-        
-        cursor.execute('SELECT * FROM usuarios WHERE email_usuario = %s', (email,))
-        existe = cursor.fetchone()
-        
-        if existe:
+        cursor.execute(
+            'select * from usuarios where email_usuario =%s  and senha_usuario=%s', (email, senha))
+        existe= cursor.fetchone()
+        if not existe:
+            return render_template('login.html', erro='Usuário não cadastrado!')
+        else:
+            session['usuario'] = existe[1]
+            return redirect(url_for('main'))
+    return render_template('login.html')
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        try:
+            usuario = request.form['usuario']
+            email = request.form['cremail']
+            senha = request.form['crsenha']
+            
+            if not usuario or not email or not senha:
+                return 'Todos os campos são obrigatórios!'
+            
+            conexao = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='guriSQL_',
+                database='expotech'
+            )
+            
+            cursor = conexao.cursor()
+            
+            cursor.execute('SELECT * FROM usuarios WHERE email_usuario = %s', (email,))
+            existe = cursor.fetchone()
+            
+            if existe:
+                cursor.close()
+                conexao.close()
+                return 'Usuário já cadastrado!'
+            
+            cursor.execute(
+                'INSERT INTO usuarios (nome_usuario, email_usuario, senha_usuario) VALUES (%s, %s, %s)',
+                (usuario, email, senha)
+            )
+            conexao.commit()
             cursor.close()
             conexao.close()
-            return 'Usuário já cadastrado!'
-        
-        cursor.execute(
-            'INSERT INTO usuarios (nome_usuario, email_usuario, senha_usuario) VALUES (%s, %s, %s)',
-            (usuario, email, senha)
-        )
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-        session['usuario']= usuario
-        return redirect(url_for('main'))
-    except Exception as e:
-        return f'Erro ao cadastrar: {str(e)}'
-
+            session['usuario']= usuario
+            return redirect(url_for('main'))
+        except Exception as e:
+            return f'Erro ao cadastrar: {str(e)}'
+    return render_template('cadastro.html')
 @app.route('/main')
 def main():
     usuario= session.get('usuario')
     return render_template('main.html', usuario=usuario)
-      
 
 if __name__ == '__main__':
     app.run(debug=True)
